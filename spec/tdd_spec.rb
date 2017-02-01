43d1d13052ec0230fe275eea5631ebedfbf5bf7a
Jason Reeder <info@jasonreeder.com>
2017-02-01 11:23:08 -0800

    Fill out #two_sum specs

--- spec/tdd_spec.rb
+++ spec/tdd_spec.rb
@@ -18,14 +18,22 @@ describe '#Array' do
   describe '#two_sum' do
     let(:array_with_sums) { [-1, 0, 2, -2, 1] }
     let(:idxs_array_with_sums) { [[0, 4], [2, 3]] }
-    subject { array_with_sums_two_sum }
+    subject { array_with_sums.two_sum }
 
-    it 'returns an array'
+    it 'returns an array' do
+      expect(subject).to be_an(Array)
+    end
 
-    it 'returns an array of arrays'
+    it 'returns an array of arrays' do
+      expect(subject.first).to be_an(Array)
+    end
 
-    it 'finds the exact number of indexes that add to zero'
+    it 'finds the exact number of indexes that add to zero' do
+      expect(subject.length).to eq(2)
+    end
 
-    it 'finds the exact idxs that add to zero'
+    it 'finds the exact idxs that add to zero' do
+      expect(subject).to eq(idxs_array_with_sums)
+    end
   end
 end
