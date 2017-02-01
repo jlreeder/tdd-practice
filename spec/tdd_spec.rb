94204ce3530891911f97dcbd49c34ab625078e2f
Jason Reeder <info@jasonreeder.com>
2017-02-01 10:58:25 -0800

    Add specs for #my_uniq

--- spec/tdd_spec.rb
+++ spec/tdd_spec.rb
@@ -1,1 +1,11 @@
 require 'tdd'
+
+describe '#array' do
+  describe '#my_uniq' do
+    it 'returns an array'
+
+    it 'returns an array with no duplicates'
+
+    it 'returns the same as Kernel#uniq'
+  end
+end
