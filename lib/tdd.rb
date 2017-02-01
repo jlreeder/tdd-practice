88332cdd680c33ede49e1fd1c54808fc62dc4522
Jason Reeder <info@jasonreeder.com>
2017-02-01 11:13:10 -0800

    Build #my_uniq method

--- lib/tdd.rb
+++ lib/tdd.rb
@@ -1,5 +1,15 @@
 class Array
   def my_uniq
-    self
+    uniques = []
+    each { |el| uniques << el unless uniques.include?(el) }
+    uniques
+  end
+
+  # Q: Is there a simpler way to phrase this with 'inject' than below?
+  def my_uniq
+    inject([]) do |uniques, el|
+      uniques << el unless uniques.include?(el)
+      uniques
+    end
   end
 end
