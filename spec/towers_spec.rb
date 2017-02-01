62342a3de178f28eedd90841bf5582ec96ab41de
Jason Reeder <info@jasonreeder.com>
2017-02-01 14:23:38 -0800

    Fill out specs for #initialize

--- spec/towers_spec.rb
+++ spec/towers_spec.rb
@@ -1,14 +1,24 @@
 require 'towers'
 
 describe 'TowersOfHanoi' do
+  subject(:game) { TowersOfHanoi.new }
+
   describe '#initialize' do
-    it 'sets up a grid'
+    it 'sets up an accessible grid of towers' do
+      expect(subject).to receive(:towers)
+    end
 
-    it 'sets up a grid with three towers'
+    it 'sets up a grid with three towers' do
+      expect(subject.towers.length).to eq(3)
+    end
 
-    it 'initializes discs'
+    it 'initializes three discs' do
+      expect(subject.towers.flatten.length).to eq(3)
+    end
 
-    it 'puts all discs on the first tower'
+    it 'puts all three discs on the first tower' do
+      expect(subject.towers[0].length).to eq([1, 2, 3])
+    end
   end
 
   describe '#move'
