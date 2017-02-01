7be7901c22a0d0efc9d5aebbeb0ed302a6088b00
Jason Reeder <info@jasonreeder.com>
2017-02-01 11:19:53 -0800

    Add #two_sum specs

--- spec/tdd_spec.rb
+++ spec/tdd_spec.rb
@@ -1,6 +1,6 @@
 require 'tdd'
 
-describe '#array' do
+describe '#Array' do
   describe '#my_uniq' do
     let(:array_with_dups) { [1, 2, 1, 3, 3] }
     let(:array_without_dups) { [1, 2, 3] }
@@ -13,5 +13,19 @@ describe '#array' do
     it 'returns an array with no duplicates' do
       expect(subject).to eq(array_without_dups)
     end
+  end
+
+  describe '#two_sum' do
+    let(:array_with_sums) { [-1, 0, 2, -2, 1] }
+    let(:idxs_array_with_sums) { [[0, 4], [2, 3]] }
+    subject { array_with_sums_two_sum }
+
+    it 'returns an array'
+
+    it 'returns an array of arrays'
+
+    it 'finds the exact number of indexes that add to zero'
+
+    it 'finds the exact idxs that add to zero'
   end
 end
