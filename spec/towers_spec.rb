fb0e9279ed90d6df5b9e8f023209f439aacd15d9
Jason Reeder <info@jasonreeder.com>
2017-02-01 14:28:31 -0800

    Refactor specs for #initialize
    For length

--- spec/towers_spec.rb
+++ spec/towers_spec.rb
@@ -1,23 +1,25 @@
 require 'towers'
 
 describe 'TowersOfHanoi' do
-  subject(:game) { TowersOfHanoi.new }
+  let(:game) { TowersOfHanoi.new }
 
   describe '#initialize' do
+    subject { game.towers }
+
     it 'sets up an accessible grid of towers' do
-      expect(subject.towers).to be_an(Array)
+      expect(subject).to be_an(Array)
     end
 
     it 'sets up a grid with three towers' do
-      expect(subject.towers.length).to eq(3)
+      expect(subject.length).to eq(3)
     end
 
     it 'initializes three discs' do
-      expect(subject.towers.flatten.length).to eq(3)
+      expect(subject.flatten.length).to eq(3)
     end
 
     it 'puts all three discs on the first tower' do
-      expect(subject.towers[0]).to eq([3, 2, 1])
+      expect(subject[0]).to eq([3, 2, 1])
     end
   end
 
